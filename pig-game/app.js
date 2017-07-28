7aefba558d11fe53ec119114d8835de1e155510a
Ling Jin <chloejin525@gmail.com>
2017-07-28 18:17:35 -0400

    additional rule no.3 added

--- pig-game/app.js
+++ pig-game/app.js
@@ -14,15 +14,15 @@ ADDITIONAL RULES:
 
 */
 
-var scores, roundScore, activePlayer, gamePlaying, previousDie, winningScore; 
+var scores, roundScore, activePlayer, gamePlaying, previousDie; 
 
-winningScore = document.querySelector(".final-score").value ;
 
 function init () {
 	scores = [0, 0];
 	roundScore = 0;
 	activePlayer = 0;
-	document.querySelector('.dice').style.display = 'none';
+	document.getElementById('dice-1').style.display = 'none';
+	document.getElementById('dice-2').style.display = 'none';
 	document.getElementById('score-0').textContent = '0';
 	document.getElementById('score-1').textContent = '0';
 	document.getElementById('current-0').textContent = '0';
@@ -45,15 +45,28 @@ init()
 document.querySelector('.btn-roll').addEventListener('click', function() {
 	if (gamePlaying) {
 		// 1. Random number
-		var dice = Math.floor(Math.random()*6) + 1;
+		var dice1 = Math.floor(Math.random()*6) + 1;
+		var dice2 = Math.floor(Math.random()*6) + 1;
 
 		// 2. Display the result
-		document.querySelector('#current-' + activePlayer).textContent = dice;
-		var diceDOM = document.querySelector('.dice');
-		diceDOM.style.display = 'block';
-		diceDOM.src = 'dice-' + dice + '.png' ;
+		document.getElementById('dice-1').style.display = 'block';
+		document.getElementById('dice-2').style.display = 'block';
+		document.getElementById('dice-1').src='dice-' + dice1 + '.png';
+		document.getElementById('dice-2').src='dice-' + dice2 + '.png';
 
 		// 3. Update the round score IF the rolled number was NOT a 1
+
+		if (dice1 !== 1 && dice2 !== 1) {
+			// Add score
+			roundScore += dice1 + dice2;
+			document.getElementById('current-' + activePlayer).textContent = roundScore;
+		}
+		else {
+			// Next player
+			// Ternarry operator 
+			nextPlayer();
+		}
+		/*
 		if (dice === 6 && previousDie === dice) {
 			scores[activePlayer] = 0;
 			document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
@@ -69,6 +82,7 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 			// Ternarry operator 
 			nextPlayer();
 		}
+		*/
 	}
 	
 });
@@ -86,7 +100,8 @@ function nextPlayer () {
 	document.querySelector('.player-0-panel').classList.toggle('active');
 	document.querySelector('.player-1-panel').classList.toggle('active');
 
-	document.querySelector('.dice').style.display='none';
+	document.getElementById('dice-1').style.display = 'none';
+	document.getElementById('dice-2').style.display = 'none';
 	
 };
 
@@ -97,6 +112,8 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
 		// Add current score to global score
 		// The variable activePlayer has been changed in the btn-roll action! 
 		scores[activePlayer] += roundScore;
+
+		var winningScore  = document.querySelector(".final-score").value; 
 
 		// Update the UI
 		document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
